1a3c6cde9efff87321d13b21f59367483311fe58
Joe Barnett <joe.barnett@creatoriq.com>
2023-03-20 16:26:18 -0700

    initial commit

---
+++ graphql/src/main/kotlin/com/joe/menforcer426/Test.kt
@@ -0,0 +1,7 @@
+package com.joe.menforcer426
+
+import com.trib3.graphql.websocket.GraphQLWebSocketSubProtocol
+
+class Test {
+    val protocol = GraphQLWebSocketSubProtocol.GRAPHQL_WS_PROTOCOL
+}
---
+++ server/src/main/kotlin/com/joe/menforcer426/ServerTest.kt
@@ -0,0 +1,8 @@
+package com.joe.menforcer426
+
+import com.trib3.graphql.websocket.GraphQLWebSocketSubProtocol
+
+class ServerTest {
+    val protocol = Test().protocol
+    val aprotocol = GraphQLWebSocketSubProtocol.APOLLO_PROTOCOL
+}
